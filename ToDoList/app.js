b2bfabb7412779c582d5ab00b8fefd7459cc22b0
SalaiThomas <salaithomas582@gmail.com>
2024-10-03 00:18:36 +0630

    addTask Function

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,12 +1,16 @@
-const { Component, mount, xml,useRef, onMounted } = owl;
+const { Component, mount, xml,useRef, onMounted , useState } = owl;
 
 class Task extends Component{
   static template = xml /* xml */`
   <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-    <input type="checkbox" t-att-checked="props.task.isCompleted"/>
+    <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
     <span><t t-esc="props.task.text"/></span>
   </div>`;
-static props = ["task"];
+  static props = ["task"];
+
+  toggleTask(){
+  this.props.task.isCompleted = !this.props.task.isCompleted
+  }
 }
 
 class Root extends Component {
@@ -22,33 +26,35 @@ class Root extends Component {
 `;
 
     static components = {Task};
-
+    nextId = 1;
+    tasks = useState([]);
     addTask(ev) {
       // 13 is keycode for ENTER
       if (ev.keyCode === 13) {
           const text = ev.target.value.trim();
           ev.target.value = "";
-          console.log('adding task', text);
-          // todo
+          if (text){
+            const newTask = {
+              id:this.nextId++,
+              text:text,
+              isCompleted:false,
+            };
+            this.tasks.push(newTask);
+          }
       }
   }
 
+
+
   setup() {
     const inputRef = useRef("add-input");
-    onMounted(() => inputRef.el.focus());
+    
+    onMounted(() => {inputRef.el.focus();
+
+      console.log(inputRef.el);
+    });
+    
 }
-  
-    tasks = [
-      {
-        id: 1,
-        text: "buy milk",
-        isCompleted: true,
-      },
-      {
-        id: 2,
-        text: "clean house",
-        isCompleted: false,
-      },
-    ];
+
   }
   mount(Root, document.body, {dev: true});
